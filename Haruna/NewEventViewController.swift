be48c6c239ac43074898ccaefd933ac74d7a5ba9
fktan5 <email@fktan5.net>
2017-05-11 19:24:33 +0900

    add NewEventcontroller

---
+++ Haruna/NewEventViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewEventViewController.swift
+//  Haruna
+//
+//  Created by keigo on 2017/05/11.
+//  Copyright © 2017年 tan5. All rights reserved.
+//
+
+import UIKit
+
+class NewEventViewController : UIViewController {
+    
+}
