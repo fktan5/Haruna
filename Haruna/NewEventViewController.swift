06ecd93ca194b53d37a0377fe7f3391baa99121a
fktan5 <email@fktan5.net>
2017-05-16 10:30:00 +0900

    add Form base

--- Haruna/NewEventViewController.swift
+++ Haruna/NewEventViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import Eureka
 
-class NewEventViewController : UIViewController {
+class NewEventViewController : FormViewController {
 
     @IBOutlet weak var CancelButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        form +++ Section("Event Info")
+            <<< TextRow() {
+                $0.title = "Event Name"
+                $0.placeholder = "Input"
+            }
+
+            <<< DateRow() { $0.value = Date(); $0.title = "Date" }
+
+
+        +++ Section("CheckList")
+
+        +++ Section("Memo")
+            <<< TextAreaRow() {
+                $0.title = "Memo"
+                $0.placeholder = "Input text here"
+        }
     }
 
     override func didReceiveMemoryWarning() {
