7cbf198aa96a44cdcebaa5224b32c80f16e020b2
fktan5 <email@fktan5.net>
2017-05-19 16:05:25 +0900

    Listing objects

--- Haruna/FirstViewController.swift
+++ Haruna/FirstViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import RealmSwift
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var items:Results<Event>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        let realm = try! Realm()
+        items = realm.objects(Event.self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,15 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        print(items[indexPath.row].name)
+        cell.textLabel?.text = items[indexPath.row].name
+        return cell
+    }
 }
 
