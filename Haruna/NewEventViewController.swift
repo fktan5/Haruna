0a4a9315f74c59b3327070235ae383b1cb1b977b
fktan5 <email@fktan5.net>
2017-05-19 19:14:47 +0900

    add new form and callback

--- Haruna/NewEventViewController.swift
+++ Haruna/NewEventViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 import Eureka
 import CoreLocation
+import RealmSwift
 
 class NewEventViewController : FormViewController {
 
     @IBOutlet weak var CancelButton: UIBarButtonItem!
     @IBOutlet weak var saveButton: UIBarButtonItem!
+
+    var event:Event = Event()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,28 +25,50 @@ class NewEventViewController : FormViewController {
             <<< TextRow() {
                 $0.title = "Event Name"
                 $0.placeholder = "Input"
-            }
+            }.onChange({ (row) in
+                self.event.name = row.value!
+            })
 
             <<< DateRow() {
                 $0.value = Date()
-                $0.title = "Date" }
+                $0.title = "Date"
+            }.onChange({(row) in
+                self.event.datetime = row.value! as NSDate
+            })
+
+            <<< ActionSheetRow<String>{
+                $0.title = "User Type"
+                $0.selectorTitle = "Select User Type"
+                $0.baseValue = "Man"
+                $0.options = ["Man", "Female"]
+                }.onChange({ (row) in
+                    self.event.user = row.value!
+                })
 
             <<< ActionSheetRow<String> {
                 $0.title = "Event Style"
                 $0.selectorTitle = "Select Style"
+                $0.baseValue = "Reception"
                 $0.options = ["Reception", "After Party"]
-            }
+            }.onChange({ (row) in
+                self.event.type = row.value!
+            })
 
         +++ Section("Access")
             <<< TextRow() {
                 $0.title = "Place Name"
                 $0.placeholder = "Input"
-            }
-            
+            }.onChange({ (row) in
+                self.event.placeName = row.value!
+            })
+
             <<< LocationRow(){
                 $0.title = "Place"
                 $0.value = CLLocation(latitude: 35.6870358, longitude: 139.7012083)
-            }
+            }.onChange({ (row) in
+                self.event.locationLat = (row.value?.coordinate.latitude)!
+                self.event.locationLng = (row.value?.coordinate.longitude)!
+            })
 
         +++ Section("CheckList")
 
@@ -63,6 +88,11 @@ class NewEventViewController : FormViewController {
     }
 
     @IBAction func touchedSaveButton(_ sender : UIBarButtonItem){
-        NSLog("Save");
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(event)
+        }
+
+        self.dismiss(animated: true)
     }
 }
