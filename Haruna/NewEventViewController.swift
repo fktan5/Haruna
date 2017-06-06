a8fc9413b4235d5aa693948ba604d28637dc021a
fktan5 <email@fktan5.net>
2017-06-06 18:50:08 +0900

    reform realm using

--- Haruna/NewEventViewController.swift
+++ Haruna/NewEventViewController.swift
@@ -22,58 +22,86 @@ class NewEventViewController : FormViewController {
 
     var event:Event = Event()
 
+    var id:String = ""
+
     var savedDelegate: OnSaveEventDelegate?
+
+    let realm = try! Realm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if(!id.isEmpty){
+            print("id:", id)
+            event = realm.object(ofType: Event.self, forPrimaryKey: id)!
+            print("Event obj:", event.name)
+        } else {
+            print("id is Empty (new Item")
+        }
 
         form +++ Section("Event Info")
             <<< TextRow() {
                 $0.title = "Event Name"
                 $0.placeholder = "Input"
+                $0.value = event.name.isEmpty ? "" : event.name
             }.onChange({ (row) in
-                self.event.name = row.value == nil ? "" : row.value!
+                try! self.realm.write {
+                    self.event.name = row.value == nil ? "" : row.value!
+                }
             })
 
             <<< DateRow() {
-                $0.value = Date()
+                $0.value = event.datetime as Date
                 $0.title = "Date"
             }.onChange({(row) in
-                self.event.datetime = row.value! as NSDate
+                try! self.realm.write {
+                    self.event.datetime = row.value! as NSDate
+                }
             })
 
             <<< ActionSheetRow<String>{
                 $0.title = "User Type"
                 $0.selectorTitle = "Select User Type"
-                $0.baseValue = "Man"
+                $0.baseValue = event.user.isEmpty ? "Man" : event.user
                 $0.options = ["Man", "Female"]
-                }.onChange({ (row) in
+            }.onChange({ (row) in
+                try! self.realm.write {
                     self.event.user = row.value!
-                })
+                }
+            })
 
             <<< ActionSheetRow<String> {
                 $0.title = "Event Style"
                 $0.selectorTitle = "Select Style"
-                $0.baseValue = "Reception"
+                $0.baseValue = event.type.isEmpty ? "Reception" : event.type
                 $0.options = ["Reception", "After Party"]
             }.onChange({ (row) in
-                self.event.type = row.value!
+                try! self.realm.write {
+                    self.event.type = row.value!
+                }
             })
 
         +++ Section("Access")
             <<< TextRow() {
                 $0.title = "Place Name"
                 $0.placeholder = "Input"
+                $0.value = event.placeName.isEmpty ? "" : event.placeName
             }.onChange({ (row) in
-                self.event.placeName = row.value == nil ? "" : row.value!
+                try! self.realm.write {
+                    self.event.placeName = row.value == nil ? "" : row.value!
+                }
             })
 
             <<< LocationRow(){
                 $0.title = "Place"
-                $0.value = CLLocation(latitude: 35.6870358, longitude: 139.7012083)
+                $0.value = id.isEmpty ?
+                    CLLocation(latitude: 35.6870358, longitude: 139.7012083)
+                    : CLLocation(latitude: event.locationLat, longitude: event.locationLng)
             }.onChange({ (row) in
-                self.event.locationLat = (row.value?.coordinate.latitude)!
-                self.event.locationLng = (row.value?.coordinate.longitude)!
+                try! self.realm.write {
+                    self.event.locationLat = (row.value?.coordinate.latitude)!
+                    self.event.locationLng = (row.value?.coordinate.longitude)!
+                }
             })
 
         +++ Section("CheckList")
@@ -82,7 +110,12 @@ class NewEventViewController : FormViewController {
             <<< TextAreaRow() {
                 $0.title = "Memo"
                 $0.placeholder = "Input text here"
-        }
+                $0.value = event.memo.isEmpty ? "" : event.memo
+            }.onChange({row in
+                try! self.realm.write {
+                    self.event.memo = row.value == nil ? "" : row.value!
+                }
+            })
     }
 
     override func didReceiveMemoryWarning() {
@@ -94,11 +127,10 @@ class NewEventViewController : FormViewController {
     }
 
     @IBAction func touchedSaveButton(_ sender : UIBarButtonItem){
-        let realm = try! Realm()
         try! realm.write {
             realm.add(event)
         }
-
+        print("new Item id", event.id)
         savedDelegate?.saved()
         self.dismiss(animated: true)
     }
