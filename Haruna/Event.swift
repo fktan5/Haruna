ca3e2a5cf02f3fb8f8e5ff45d4a929ceace2094b
fktan5 <email@fktan5.net>
2017-06-09 18:05:50 +0900

    added ChekcList Item and relation

--- Haruna/Event.swift
+++ Haruna/Event.swift
@@ -18,8 +18,9 @@ class Event: Object {
     dynamic var locationLat:Double = 0.0
     dynamic var locationLng:Double = 0.0
     dynamic var datetime:NSDate = NSDate()
-//    dynamic var checklist:List = List<CheckItem>()
     dynamic var memo:String = ""
+
+    let checklist = List<CheckItem>()
 
     override static func primaryKey() -> String? {
         return "id"
@@ -27,5 +28,9 @@ class Event: Object {
 }
 
 class CheckItem:Object {
+    dynamic var id:String = UUID().uuidString
     dynamic var name:String = ""
+    dynamic var isGet:Bool = false
+
+    let event = LinkingObjects(fromType: Event.self, property: "checklist")
 }
